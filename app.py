79c6d26809f1b3938ffc79baf0763b6c0efa80ef
Diksha <dikshawanjari03@gmail.com>
2022-03-25 12:08:15 +0530

    dockerfile and buildspecial is created

--- app.py
+++ app.py
@@ -3,3 +3,39 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from time import sleep
 import re
+headers = {'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0','Accept-Language': 'en-US, en;q=0.5'}
+search_query = 'mobile'.replace(' ','+')
+for i in search_query:
+    base_url = 'https://www.amazon.in/s?k={0}'.format(search_query)
+    items = []
+    for i in range(1, 2):
+        print('Processing {0}...'.format(base_url + '&page={0}'.format(i)))
+        response = requests.get(base_url + '&page={0}'.format(i), headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        results = soup.find_all('div', {'class': 's-result-item', 'data-component-type': 's-search-result'})
+
+        for result in results:
+            product_name = result.h2.text
+
+            try:
+                rating = result.find('i', {'class': 'a-icon'}).text
+                rating_count = result.find('span', {'class': 'a-size-base'}).text
+            except AttributeError:
+                continue
+
+            try:
+                price1 = result.find('span', {'class': 'a-price-whole'}).text
+                price2 = result.find('span', {'class': "a-price a-text-price"}).text
+                price2 = re.sub("^₹.*₹", "_", price2).strip("_")
+
+                product_url = 'https://amazon.in' + result.h2.a['href']
+                #print(product_url)
+                items.append([product_name, rating, rating_count, price1, price2, product_url])
+
+            except AttributeError:
+                continue
+        sleep(1.5)
+    print(items)
+
+
